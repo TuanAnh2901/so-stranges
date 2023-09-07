34aecd3e6dd340cbe33528432cd842d87789c221
Tuan Anh <tuananh4c.ltk@gmail.com>
2023-09-07 14:36:15 +0700

    a

--- Auto chat.py
+++ Auto chat.py
@@ -6,11 +6,14 @@ import string
 print("Nhớ chỉnh qua Vietnam unikey khi nhập số")
 number = int(input("Enter the number: "))
 time.sleep(5)
+test2 = ["I", "Don't", "Know"]
 e = "I Love You"
 n = 1
+test = "Cust"
 
 for i in range(number):
         s = string.ascii_lowercase
-        gui.typewrite(str(n) + ". " + e)
+        gui.typewrite(random.choice(test2))
+        ##gui.typewrite(str(n) + ". " + e)
         gui.press("Enter")
-        n = n + 1
+        ##n = n + 1
